8314be6309acf3f3fdbe287f5eef9ac947cf3f42
pierre crochet <pierrecrochet03@gmail.com>
2020-12-09 17:11:42 +0000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_09_125046) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "kind"
